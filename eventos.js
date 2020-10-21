0cd967dae76bd7c7b65648e08b8ca4942600fe8a
JemDAW <jemimamolina.alu@iespacomolla.es>
2020-10-21 10:41:03 +0200

    1 y 3 funcionales

--- eventos.js
+++ eventos.js
@@ -1,16 +1,53 @@
+//1
+
+window.onmousemove = function (){
+    var x = window.event.clientX;
+    var y = window.event.clientY;
+
+
+ document.getElementById("raton").innerText = "x: "+x+" y: "+y;
+ 
+}
+
+//2
+/*
 var intervalo;
-window.addEventListener("load", activarScript(), false);
+window.addEventListener("load", activarScript, false);
 
 function activarScript(){
 
-    window.addEventListener("click", comenzarSaludo(), false);
-    window.addEventListener("click", pararSaludo(), false);
+    window.addEventListener("click", comenzarSaludo, false);
+    window.addEventListener("click", pararSaludo, false);
 }
 
 function comenzarSaludo(){
-   intervalo= setInterval()
+   intervalo= setInterval();
 }
 
 function pararSaludo(){
     clearInterval(intervalo);
 }
+*/
+//3
+
+function generarLetra(){
+	var letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+	var numero = (Math.random()*15).toFixed(0);
+	return letras[numero];
+}
+	
+function colorHEX(){
+	var coolor = "";
+	for(var i=0;i<6;i++){
+		coolor = coolor + generarLetra() ;
+	}
+	return "#" + coolor;
+}
+
+function cambiarColor(){
+    document.getElementById("fondo").style.backgroundColor= colorHEX();
+}
+
+window.addEventListener("dblclick", cambiarColor, false);
+
+//4
